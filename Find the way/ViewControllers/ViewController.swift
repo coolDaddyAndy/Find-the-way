159e4a46eb17bb1ffbc7fded8c3f7fd3cd4737a7
Andrey Sushkov <>
2023-03-14 16:13:33 +0300

    mapView with config added

--- Find the way/ViewControllers/ViewController.swift
+++ Find the way/ViewControllers/ViewController.swift
@@ -6,12 +6,37 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
+
 
 class ViewController: UIViewController {
+    
+    private let mapView: MKMapView = {
+       let map = MKMapView()
+        map.translatesAutoresizingMaskIntoConstraints = false
+        return map
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
     
+        setConstraints()
+    }
+}
+
+
+extension ViewController {
+    
+    private func setConstraints() {
+        
+        view.addSubview(mapView)
+        NSLayoutConstraint.activate([
+            mapView.topAnchor.constraint(equalTo: view.topAnchor, constant: 0),
+            mapView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
+            mapView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
+            mapView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0)
+        ])
     }
 }
 
